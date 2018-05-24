741682816e7eed58d5b6c5b917c6a081641d4df9
dop3ch3f <ifeanyi.ibekie@gmail.com>
2018-05-24 10:49:45 +0100

    fixed the environment variables

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt-nodejs');
 const knex = require('knex')({
     client: "pg",
     connection: {
-        host: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         ssl: true
     }
 });
